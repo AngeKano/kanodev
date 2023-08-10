c6c161aa527ac22d7949838a432a3083deeb4d8d
AngeKano <Angekano002@gmail.com>
2023-08-10 00:03:33 +0000

    VV10/08/23

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,16 +5,16 @@ const Contact = () => {
   return (
     <main className="flex min-h-fit  justify-center">
       <div className="flex items-center justify-center gap-4  h-96 w-4/5">
-        <Link 
-        href={"/"} className="h-full  flex flex-1 flex-col gap-y-10 bg-first-color rounded-2xl items-center justify-center  ">
+        <Link
+          href={"/"}
+          className="h-full  flex flex-1 flex-col gap-y-10 bg-first-color rounded-2xl items-center justify-center  "
+        >
           <div className="w-36 h-36 rounded-full bg-yellow-600"></div>
           <span className="font-semibold text-xl text-white ">
             First section
           </span>
         </Link>
-        <div className="h-full contactActive rounded-2xl ">
-          Second Section
-        </div>
+        <div className="h-full contactActive rounded-2xl "></div>
         <Link
           href={"/"}
           className="h-full flex flex-1 flex-col gap-y-10 bg-first-color rounded-2xl items-center justify-center "
