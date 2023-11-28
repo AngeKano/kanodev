a09d229d6e20c0d4f20445890e1339496ca8ce45
AngeKano <Angekano002@gmail.com>
2023-11-28 23:59:16 +0000

    VV 4564

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
 
   return (
     <main className="flex min-h-fit justify-center">
-      <div className="flex items-center justify-center gap-4 h-96 w-4/5">
+      <div className="flex items-center justify-center gap-4 w-4/5">
         <button
           onClick={() => {
             setActive(1);
@@ -21,18 +21,37 @@ const Contact = () => {
           <div className="w-36 h-36 rounded-full bg-yellow-600"></div>
           <span className="font-semibold text-xl text-white ">aezazaze</span>
         </button>
-
         <button
           onClick={() => {
             setActive(2);
           }}
           className={
             active == 2
-              ? "h-full flex flex-[3] bg-amber-700 rounded-2xl transition-all ease-in "
-              : "h-full flex flex-1 bg-amber-700 rounded-2xl transition-all ease-in "
+              ? "h-full flex flex-col p-4 gap-y-4 items-center justify-center flex-[3] rounded-2xl transition-all ease-in cursor-none -z-10 "
+              : "h-full flex flex-col p-4  justify-center flex-1 rounded-2xl transition-all ease-in "
           }
         >
-          Ange KANO 002
+          <span className="font-semibold text-2xl">
+            Laissez un commentaire 😇
+          </span>
+          <div className="flex flex-col items-start  gap-y-2 w-full">
+            <span className="text-xl font-medium">Email 📧</span>
+            <input
+              type="email"
+              className="w-full rounded-full py-2 px-3 outline-none  "
+            />
+          </div>
+          <div className="flex flex-col items-start gap-y-2 w-full">
+            <span className="text-xl font-medium">Merci d'avance 👋</span>
+            <textarea
+              name=""
+              id=""
+              className="rounded-lg w-full h-40 py-2 px-3 outline-none"
+            ></textarea>
+          </div>
+          <div className="flex-col flex items-start ">
+            <button className="p-3 bg-red-500">Envoyer</button>
+          </div>
         </button>
         <button
           onClick={() => {
