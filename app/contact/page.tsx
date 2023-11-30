1b67cfadbe7e945d7134cfb7cc9d5b8d8e76a807
AngeKano <Angekano002@gmail.com>
2023-11-30 06:35:14 +0000

    VV 30/11/23

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Contact = () => {
   const [active, setActive] = useState(2);
@@ -21,57 +21,110 @@ const Contact = () => {
           <div className="w-36 h-36 rounded-full bg-yellow-600"></div>
           <span className="font-semibold text-xl text-white ">aezazaze</span>
         </button>
-        <button
-          onClick={() => {
-            setActive(2);
-          }}
-          className={
-            active == 2
-              ? "h-full flex flex-col p-4 gap-y-4 items-center justify-center flex-[3] rounded-2xl transition-all ease-in cursor-none "
-              : "h-full flex flex-col p-4  justify-center flex-1 rounded-2xl transition-all ease-in "
-          }
-        >
-          <span className="font-semibold text-2xl">
-            Laissez un commentaire 😇
-          </span>
-          <div className="flex flex-col items-start  gap-y-2 w-full">
-            <span className="text-xl font-medium">Email 📧</span>
-            <input
-              type="email"
-              className="w-full rounded-full py-2 px-3 outline-none  "
-            />
-          </div>
-          <div className="flex flex-col items-start gap-y-2 w-full">
-            <span className="text-xl font-medium">Merci d'avance 👋</span>
-            <textarea
-              name=""
-              id=""
-              placeholder="Commentaire"
-              className="rounded-lg w-full h-40 py-2 px-3 outline-none"
-            ></textarea>
-          </div>
-          <div className="flex-row gap-x-3 flex items-start ">
-            <button className="py-2 px-4 bg-dark-color rounded-full font-medium text-white ">
-              ENVOYER
-            </button>
-            <button className="svg py-2 px-2 bg-gray-100 rounded-full font-medium text-dark-color  ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6  hover:rotate-90 transition-all ease-out  "
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-        </button>
+
+        {/* : "h-full flex flex-col p-4  justify-center flex-1 rounded-2xl transition-all ease-in " */}
+        {active == 2 ? (
+          <button
+            onClick={() => {
+              setActive(2);
+            }}
+            className={
+              "h-full flex flex-col p-4 gap-y-4 items-center justify-center flex-[3] rounded-2xl transition-all ease-in cursor-none "
+            }
+          >
+            <span className="font-semibold text-2xl">
+              Laissez un commentaire 😇
+            </span>
+            <div className="flex flex-col items-start  gap-y-2 w-full">
+              <span className="text-xl font-medium">Email 📧</span>
+              <input
+                type="email"
+                className="w-full rounded-full py-2 px-3 outline-none  "
+              />
+            </div>
+            <div className="flex flex-col items-start gap-y-2 w-full">
+              <span className="text-xl font-medium">Merci d'avance 👋</span>
+              <textarea
+                name=""
+                id=""
+                placeholder="Commentaire"
+                className="rounded-lg w-full h-40 py-2 px-3 outline-none"
+              ></textarea>
+            </div>
+            <div className="flex-row gap-x-3 flex items-start ">
+              <button className="py-2 px-4 bg-dark-color rounded-full font-medium text-white ">
+                ENVOYER
+              </button>
+              <button className="svg py-2 px-2 bg-gray-100 rounded-full font-medium text-dark-color  ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6  hover:rotate-90 transition-all ease-out  "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setActive(2);
+            }}
+            className={
+              "h-full flex flex-col p-4  justify-center flex-1 rounded-2xl transition-all ease-in "
+            }
+          >
+            <span className="font-semibold text-2xl">
+              Laissez un commentaire 😇
+            </span>
+            <div className="flex flex-col items-start  gap-y-2 w-full">
+              <span className="text-xl font-medium">Email 📧</span>
+              <input
+                type="email"
+                className="w-full rounded-full py-2 px-3 outline-none  "
+              />
+            </div>
+            <div className="flex flex-col items-start gap-y-2 w-full">
+              <span className="text-xl font-medium">Merci d'avance 👋</span>
+              <textarea
+                name=""
+                id=""
+                placeholder="Commentaire"
+                className="rounded-lg w-full h-40 py-2 px-3 outline-none"
+              ></textarea>
+            </div>
+            <div className="flex-row gap-x-3 flex items-start ">
+              <button className="py-2 px-4 bg-dark-color rounded-full font-medium text-white ">
+                ENVOYER
+              </button>
+              <button className="svg py-2 px-2 bg-gray-100 rounded-full font-medium text-dark-color  ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6  hover:rotate-90 transition-all ease-out  "
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </button>
+        )}
+
         <button
           onClick={() => {
             setActive(3);
