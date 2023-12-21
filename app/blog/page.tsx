ba31e998e482938e32d1c7e739a2da042b5ffdc6
AngeKano <Angekano002@gmail.com>
2023-12-21 23:44:10 +0000

    VV 21/12/23

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog  bientot avec cvcvbeaucoup plus d'interaction 😄</div>;
+  return <div>Blog  vcfddffdfdbientot avec cvcvbeaucoup plus d'interaction 😄</div>;
 };
 
 export default Blog;
