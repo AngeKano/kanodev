e034c2c993c2479d46c92455771d271fd6f1a042
AngeKano <Angekano002@gmail.com>
2024-02-07 23:21:59 +0000

    VV 7/2/24

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog bientot avec beaucoup plus d'interaction 😄</div>;
+  return <div></div><div>Blog bientot avec beaucoup plus d'interaction 😄</div>;
 };
 
 export default Blog;
