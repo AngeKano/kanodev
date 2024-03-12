0a07b2dd0116fd4cb889d97c96816b3aef41e5e1
AngeKano <Angekano002@gmail.com>
2024-03-12 12:42:56 +0000

    #Add Speed Insight # 12/3/24

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import Navbar from "./components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Navtop from "./components/Navtop";
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
           </div>
 
           {children}
+          <SpeedInsights />
           <Navbar />
         </section>
       </body>
